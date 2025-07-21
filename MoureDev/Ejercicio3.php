7025ce387b561a45ba2da60e8e85ac2cca72f52a
saul1023 <saul10531725@gmail.com>
2025-07-21 10:01:45 -0400

    Se realizo el merge de la rama 'RamaEjercicio'

--- MoureDev/Ejercicio3.php
+++ MoureDev/Ejercicio3.php
@@ -2,7 +2,11 @@
 for($i=2; $i<=100; $i++){
     if($i%2==0){
         echo "$i es par\n";
+        $count++;
     } else {
         echo "$i es impar\n";
+        $count_impar++;
     }
 }
+echo "Total de números pares: $count\n";
+echo "Total de números impares: $count_impar\n";
